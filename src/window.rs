af80d229f54b7c8e1c75575ff2dfc04a60e5d691
Jannis Becker <31903525+moka491@users.noreply.github.com>
2021-11-07 17:06:20 +0100

    move ui code to separate module

---
+++ src/window.rs
@@ -0,0 +1,190 @@
+use std::sync::mpsc::Receiver;
+
+use glium::{
+    glutin::{
+        dpi::LogicalSize,
+        event::{Event, StartCause, WindowEvent},
+        event_loop::{ControlFlow, EventLoop},
+        window::WindowBuilder,
+        ContextBuilder,
+    },
+    index::PrimitiveType,
+    texture::{RawImage2d, SrgbTexture2d},
+    Display, IndexBuffer, Program, Surface, VertexBuffer,
+};
+
+#[derive(Copy, Clone)]
+struct Vertex {
+    position: [f32; 2],
+    tex_coords: [f32; 2],
+}
+
+fn create_vertex_buffer(display: &Display) -> VertexBuffer<Vertex> {
+    {
+        implement_vertex!(Vertex, position, tex_coords);
+
+        VertexBuffer::new(
+            display,
+            &[
+                Vertex {
+                    position: [-1.0, -1.0],
+                    tex_coords: [0.0, 0.0],
+                },
+                Vertex {
+                    position: [-1.0, 1.0],
+                    tex_coords: [0.0, 1.0],
+                },
+                Vertex {
+                    position: [1.0, 1.0],
+                    tex_coords: [1.0, 1.0],
+                },
+                Vertex {
+                    position: [1.0, -1.0],
+                    tex_coords: [1.0, 0.0],
+                },
+            ],
+        )
+        .unwrap()
+    }
+}
+fn create_index_buffer(display: &Display) -> IndexBuffer<u16> {
+    IndexBuffer::new(display, PrimitiveType::TriangleStrip, &[1 as u16, 2, 0, 3]).unwrap()
+}
+fn create_shader_program(display: &Display) -> Program {
+    program!(display,
+        140 => {
+            vertex: "
+                #version 140
+                uniform mat4 matrix;
+                in vec2 position;
+                in vec2 tex_coords;
+                out vec2 v_tex_coords;
+                void main() {
+                    gl_Position = matrix * vec4(position, 0.0, 1.0);
+                    v_tex_coords = tex_coords;
+                }
+            ",
+
+            fragment: "
+                #version 140
+                uniform sampler2D tex;
+                in vec2 v_tex_coords;
+                out vec4 f_color;
+                void main() {
+                    f_color = texture(tex, v_tex_coords);
+                }
+            "
+        },
+
+        110 => {
+            vertex: "
+                #version 110
+                uniform mat4 matrix;
+                attribute vec2 position;
+                attribute vec2 tex_coords;
+                varying vec2 v_tex_coords;
+                void main() {
+                    gl_Position = matrix * vec4(position, 0.0, 1.0);
+                    v_tex_coords = tex_coords;
+                }
+            ",
+
+            fragment: "
+                #version 110
+                uniform sampler2D tex;
+                varying vec2 v_tex_coords;
+                void main() {
+                    gl_FragColor = texture2D(tex, v_tex_coords);
+                }
+            ",
+        },
+
+        100 => {
+            vertex: "
+                #version 100
+                uniform lowp mat4 matrix;
+                attribute lowp vec2 position;
+                attribute lowp vec2 tex_coords;
+                varying lowp vec2 v_tex_coords;
+                void main() {
+                    gl_Position = matrix * vec4(position, 0.0, 1.0);
+                    v_tex_coords = tex_coords;
+                }
+            ",
+
+            fragment: "
+                #version 100
+                uniform lowp sampler2D tex;
+                varying lowp vec2 v_tex_coords;
+                void main() {
+                    gl_FragColor = texture2D(tex, v_tex_coords);
+                }
+            ",
+        },
+    )
+    .unwrap()
+}
+
+pub fn render_window(width: u32, height: u32, frame_grabber_rx: Receiver<Vec<u8>>) {
+    let event_loop = EventLoop::new();
+    let display = Display::new(
+        WindowBuilder::new().with_inner_size(LogicalSize::new(width, height)),
+        ContextBuilder::new().with_vsync(false),
+        &event_loop,
+    )
+    .unwrap();
+
+    let vertex_buffer = create_vertex_buffer(&display);
+    let index_buffer = create_index_buffer(&display);
+    let program = create_shader_program(&display);
+
+    event_loop.run(move |ev, _, control_flow| {
+        match ev {
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::CloseRequested => {
+                    *control_flow = ControlFlow::Exit;
+                    return;
+                }
+                _ => return,
+            },
+            Event::NewEvents(cause) => match cause {
+                StartCause::ResumeTimeReached { .. } => (),
+                StartCause::Init => (),
+                _ => return,
+            },
+            Event::MainEventsCleared => {
+                let _ = frame_grabber_rx.recv().and_then(|frame| {
+                    let image = RawImage2d::from_raw_rgb_reversed(&frame, (width, height));
+                    let opengl_texture = SrgbTexture2d::new(&display, image).unwrap();
+
+                    // building the uniforms
+                    let uniforms = uniform! {
+                        matrix: [
+                            [1.0, 0.0, 0.0, 0.0],
+                            [0.0, 1.0, 0.0, 0.0],
+                            [0.0, 0.0, 1.0, 0.0],
+                            [0.0, 0.0, 0.0, 1.0f32]
+                        ],
+                        tex: &opengl_texture
+                    };
+
+                    let mut target = display.draw();
+                    target
+                        .draw(
+                            &vertex_buffer,
+                            &index_buffer,
+                            &program,
+                            &uniforms,
+                            &Default::default(),
+                        )
+                        .unwrap();
+
+                    target.finish().unwrap();
+
+                    Ok(())
+                });
+            }
+            _ => return,
+        };
+    });
+}
